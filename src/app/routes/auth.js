ee8afa354e05ddfa8a3715e75b9182cb78072236
felipebravo <felipebravotapia@live.cl>
2021-01-14 11:31:24 -0300

    add list services

--- src/app/routes/auth.js
+++ src/app/routes/auth.js
@@ -1,8 +1,25 @@
 const { Router } = require('express');
 const AuthController = require('../controller/AuthController');
 const router = Router();
+const baseUrl = `/service/${process.env.VERSION_APP}/${process.env.APP_PATH_SERVICE}`;
+const routes = [];
 
 router.post('/getToken', AuthController.getToken);
 router.get('/validateToken', AuthController.validateToken);
+
+
+/**
+ * Muestra de las distintas rutas creadas
+ */
+console.log("********* Routes Avaibles ***********");
+router.stack.forEach(middleware => {
+    if (middleware.route) {
+        let route = {};
+        route.method = Object.keys(middleware.route.methods);
+        route.path = baseUrl + middleware.route.path
+        routes.push(route);
+    }
+});
+console.table(routes);
 
 module.exports = router;
