d8f8a76ac89de81d6643d3fb5037afa5e57ae46c
felipebravo <felipebravotapia@live.cl>
2021-01-14 12:01:22 -0300

    Add control de parseo

--- src/app/app.js
+++ src/app/app.js
@@ -4,15 +4,29 @@ const app = express();
 const index = require('./routes/index');
 const auth = require('./routes/auth');
 require('dotenv').config();
-
+const http = require("../core/http");
 const baseUrl = `/${process.env.APP_PATH_SERVICE}/api/${process.env.VERSION_APP}`;
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-//app.use(baseUrl,index);
+app.use(baseUrl,index);
 app.use(baseUrl, auth);
+
+
+/**
+ * Controla Error de parseo JSON
+ */
+app.use(function (err, req, res, next) {
+    console.error("Error ", err.message);
+    const errorDetails = {
+      statusCode: 500,
+      message: err.message,
+      description: "Error desconocido. Favor intente más tarde...",
+    };
+    http.error(res, errorDetails);
+  });
 
 module.exports = app;
 
